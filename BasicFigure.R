05856f9851a030c2b91dd9d9f38bdca7f376d0d0
aestears <astears@uwyo.edu>
2023-04-12 14:51:08 -0600

    changed "smooth" to a glm

--- BasicFigure.R
+++ BasicFigure.R
@@ -7,5 +7,5 @@ head(mtcars)
 # make a figure
 ggplot(data = mtcars, aes(mpg, hp))+
   geom_point(aes(color = as.factor(gear))) + 
-  geom_smooth(aes(color = as.factor(gear)), method = "lm") + 
+  geom_smooth(aes(color = as.factor(gear)), method = "glm") + 
   theme_classic()

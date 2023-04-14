a645eae129fc04920003332d709e054c45a4aff0
aestears <astears@uwyo.edu>
2023-04-13 18:32:32 -0600

    fixing merge conflict

--- BasicFigure.R
+++ BasicFigure.R
@@ -1,11 +1,5 @@
-library(ggplot2)
-
 # download cars dataset
 data(mtcars)
-head(mtcars)
 
 # make a figure
-ggplot(data = mtcars, aes(mpg, hp))+
-  geom_point(aes(color = as.factor(gear))) + 
-  geom_smooth(aes(color = as.factor(gear)), method = "glm") + 
-  theme_classic()
+plot(mpg ~ hp, data = mtcars)

89f76307bcc3a4d1a57d167ce50ba20c1ceea761
aestears <astears@uwyo.edu>
2023-04-13 18:57:45 -0600

    resolving the revert merge conflict

--- BasicFigure.R
+++ BasicFigure.R
@@ -1,9 +1,6 @@
-library(ggplot2)
-
 # download cars dataset
 data(mtcars)
 
 # make a figure
-ggplot(mtcars) +
-  geom_point(aes(mpg, cyl, col = gear)) + 
-  theme_classic()
+
+plot(mpg ~ hp, data = mtcars)
